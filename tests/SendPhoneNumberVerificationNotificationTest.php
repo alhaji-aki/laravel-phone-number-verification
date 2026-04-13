687a8ba3f3fe8e2a06d631cb1aa9be8dbf85021a
Abdul Kudus Issah <abdulkudus2922@gmail.com>
2026-04-13 06:52:48 +0000

    Added PHP8.5 and Laravel 13 Support

--- tests/SendPhoneNumberVerificationNotificationTest.php
+++ tests/SendPhoneNumberVerificationNotificationTest.php
@@ -1,54 +1,36 @@
 <?php
 
-namespace AlhajiAki\PhoneNumberVerification\Tests;
-
 use AlhajiAki\PhoneNumberVerification\Contracts\MustVerifyPhoneNumber;
 use AlhajiAki\PhoneNumberVerification\Listeners\SendPhoneNumberVerificationNotification;
 use AlhajiAki\PhoneNumberVerification\Tests\Models\NotImplementedMustVerifyPhoneNumberModel;
 use Illuminate\Auth\Events\Registered;
 use Mockery as m;
-use PHPUnit\Framework\TestCase;
 
-class SendPhoneNumberVerificationNotificationTest extends TestCase
-{
-    /**
-     * @return void
-     */
-    public function test_user_is_an_instance_of_must_verify_phone_number_and_has_not_verified_phone_number()
-    {
-        $user = $this->getMockBuilder(MustVerifyPhoneNumber::class)->getMock();
-        $user->method('hasVerifiedPhoneNumber')->willReturn(false);
-        $user->expects($this->once())->method('generatePhoneNumberVerificationToken');
+test('user is an instance of must verify phone number and has not verified phone number', function () {
+    $user = $this->getMockBuilder(MustVerifyPhoneNumber::class)->getMock();
+    $user->method('hasVerifiedPhoneNumber')->willReturn(false);
+    $user->expects($this->once())->method('generatePhoneNumberVerificationToken');
 
-        $listener = new SendPhoneNumberVerificationNotification;
+    $listener = new SendPhoneNumberVerificationNotification;
 
-        $listener->handle(new Registered($user));
-    }
+    $listener->handle(new Registered($user));
+});
 
-    /**
-     * @return void
-     */
-    public function test_user_is_not_an_instance_of_must_verify_phone_number()
-    {
-        $user = m::mock(NotImplementedMustVerifyPhoneNumberModel::class);
-        $user->shouldNotReceive('generatePhoneNumberVerificationToken');
+test('user is not an instance of must verify phone number', function () {
+    $user = m::mock(NotImplementedMustVerifyPhoneNumberModel::class);
+    $user->shouldNotReceive('generatePhoneNumberVerificationToken');
 
-        $listener = new SendPhoneNumberVerificationNotification;
+    $listener = new SendPhoneNumberVerificationNotification;
 
-        $listener->handle(new Registered($user));
-    }
+    $listener->handle(new Registered($user));
+});
 
-    /**
-     * @return void
-     */
-    public function test_user_is_an_instance_of_must_verify_phone_number_and_has_verified_phone_number()
-    {
-        $user = $this->getMockBuilder(MustVerifyPhoneNumber::class)->getMock();
-        $user->method('hasVerifiedPhoneNumber')->willReturn(true);
-        $user->expects($this->never())->method('generatePhoneNumberVerificationToken');
+test('user is an instance of must verify phone number and has verified phone number', function () {
+    $user = $this->getMockBuilder(MustVerifyPhoneNumber::class)->getMock();
+    $user->method('hasVerifiedPhoneNumber')->willReturn(true);
+    $user->expects($this->never())->method('generatePhoneNumberVerificationToken');
 
-        $listener = new SendPhoneNumberVerificationNotification;
+    $listener = new SendPhoneNumberVerificationNotification;
 
-        $listener->handle(new Registered($user));
-    }
-}
+    $listener->handle(new Registered($user));
+});
